547d524d34f75d1401ef5202b90bb4410bb0bf25
dan <danilmelkozerov0@gmail.com>
2022-04-08 03:36:32 +0500

    no comments

--- scrapper.py
+++ scrapper.py
@@ -1,3 +1,4 @@
+from platform import release
 from re import S
 from textwrap import indent
 from bs4 import BeautifulSoup
@@ -7,49 +8,30 @@ import lxml.html
 
 def steam_scrape():
     url = requests.get('https://store.steampowered.com/explore/new/')
-    doc = BeautifulSoup(url.text, "lxml")
-    lxmldoc = lxml.html.fromstring(url.content)
-    apps = lxmldoc.xpath('//div[@id="tab_newreleases_content"]')[0]
+    doc = lxml.html.fromstring(url.content)
+    releases = doc.xpath('//div[@id="tab_newreleases_content"]')[0]
 
-    finalPriceList = []
     titleList = []
-    tagList = []
-    platformList = []
+    finalPriceList = []
     discountList = []
     originalPriceList = []
+    tagList = []
+    platformList = []
 
+    game = releases.xpath('.//a[@class="tab_item app_impression_tracked"]')
 
-    prices = doc.find_all("div", {"class" : [
-    "discount_block tab_item_discount", 
-    "discount_block tab_item_discount no_discount", 
-    "discount_block empty tab_item_discount",
-    ]})
-    for price in prices:
-        if price.text == '':
-            finalPriceList.append('Free')
-        else:
-            pure_price = price.text
-            if "%" in pure_price:
-                getDiscount = pure_price.split("%")
-                getOriginal = getDiscount[1].split("€")
-                discountList.append(getDiscount[0])
-                originalPriceList.append(getOriginal[0])
-                finalPriceList.append(getOriginal[1])
-            else:
-                discountList.append('None')
-                originalPriceList.append('None')  
-                finalPriceList.append(pure_price)
+    game_price = game.xpath('.//div[contains(@class, "discount_block")]')
 
-    titles = doc.find_all("div", class_ = "tab_item_name")
+    for price in game_price:
+        print(price.text)
+
+    titles = releases.xpath('.//div[@class="tab_item_name"]')
     for title in titles:
-        titleList.append(title.text)
+        print(title.text)
 
 
-    tags = doc.find_all("div", class_ = "tab_item_top_tags")
-    for tag in tags:
-        tagList.append(tag.text)
 
-    platforms_div = apps.xpath('.//div[@class="tab_item_details"]')
+    platforms_div = releases.xpath('.//div[@class="tab_item_details"]')
     for game in platforms_div:
         temp = game.xpath('.//span[contains(@class, "platform_img")]') 
         platforms = [t.get('class').split(' ')[-1] for t in temp]
@@ -59,15 +41,15 @@ def steam_scrape():
             
 
     output = []
-    for info in zip(titleList, finalPriceList, tagList, discountList, originalPriceList, platformList):
+    for info in zip(titleList, finalPriceList, discountList, originalPriceList, tagList, platformList):
         resp = {}
         resp['title'] = info[0]
         resp['price'] = info[1]
-        resp['discount'] = info[3]
-        resp['original-price'] = info[4]
-        resp['tags'] = info[2]
-        resp['platform'] = info[5]
-        output.append(resp)
+        # resp['discount'] = info[2]
+        # resp['original-price'] = info[3]
+        # resp['tags'] = info[4]
+        # resp['platform'] = info[5]
+        # output.append(resp)
 
     jsonObj = json.dumps(output, indent=4)
     return(jsonObj)
